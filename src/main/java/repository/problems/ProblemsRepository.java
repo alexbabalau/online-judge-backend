608bba3dbe8b45eeada58288bb58487bc7de8430
alexbabalau <alexandru.babalau@yahoo.com>
2021-02-11 15:08:58 +0200

    OJA-16 added JPA repositories

---
+++ src/main/java/repository/problems/ProblemsRepository.java
@@ -0,0 +1,16 @@
+package repository.problems;
+
+import java.util.List;
+
+import org.springframework.data.repository.Repository;
+
+import model.problems.Problem;
+
+public interface ProblemsRepository extends Repository<Problem, Long> {
+	
+	Problem save(Problem problem);
+	
+	Problem findById(Long id);
+	
+	List<Problem> findAll();
+}
---
+++ src/main/java/repository/submissions/SubmissionsRepository.java
@@ -0,0 +1,14 @@
+package repository.submissions;
+
+import org.springframework.data.repository.Repository;
+
+import model.submissions.Submission;
+
+public interface SubmissionsRepository extends Repository<Submission, Long>{
+	
+	Submission save(Submission submission);
+	
+	Submission findById(Long id);
+	
+	
+}
